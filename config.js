e645c445b2e829c5eadbf4cd6f08b4bd3a713012
Aaron Jen <aaronjen@jubo.health>
2019-08-26 22:27:37 +0800

    add GA

--- config.js
+++ config.js
@@ -8,7 +8,7 @@ module.exports = {
   copyright: '© 2019 Ching-Wei Jen All rights reserved.',
   disqusShortname: '',
   postsPerPage: 4,
-  googleAnalyticsId: 'UA-73379983-2',
+  googleAnalyticsId: 'UA-146444315-1',
   useKatex: false,
   menu: [
     {
